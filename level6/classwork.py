55fce65c1b7ed0e379522b5baf360e2660649bef
tatasadagashvili <tatasadaghashvili@gmail.com>
2024-06-21 16:54:42 +0400

    second commit

---
+++ level6/classwork.py
@@ -0,0 +1,10 @@
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: " ))
+
+print(num1 + num1, num2 * num2)
+
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: " ))
+
+print(num1 + num1, num2 * num2, num1 / num1, num2 - num2)
---
+++ level6/homework.py
@@ -0,0 +1,17 @@
+#name= input("Enter your name:" )
+#lastname= input("Enter your lastname:" )
+#age= int(input("Enter your age:" ))
+#gmail= input("Enter your gmail:" )
+#print(f"Your name is {name}, Your lastname is {lastname}, Your age is {age}, Your gmail is {gmail}".format())
+
+#num1=int(input("Enter first number: "))
+#num2=int(input("Enter second number: "))
+#num3=int(input("Enter third number: "))
+#print(num1 + num2 )
+#print(num2 / num1 )
+#print(num1 * num3 )
+#print(num2 - num3 )
+
+price= 15
+things= 90
+print(90 * 15)
